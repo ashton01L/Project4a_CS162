75b657f51de33597f6d92857ef3cd9862e983fa0
ashton01L <ashtonamazing@gmail.com>
2024-10-26 21:09:28 -0500

    fixed errors

---
+++ README.md
@@ -0,0 +1,3 @@
+Modify the binary search function from the exploration so that, instead of returning -1 when the target value is not in
+the list, raises a TargetNotFound exception (you'll need to define this exception class). Otherwise it should
+function normally.
---
+++ bin_except.py
@@ -0,0 +1,34 @@
+# Author: Ashton Lee
+# Github User: ashton01L
+# Date: 10/26/2024
+# Description: Modify the binary search function from the exploration so that, instead of returning -1 when the target
+# value is not in the list, raises a TargetNotFound exception (you'll need to define this exception class).
+# Otherwise it should function normally.
+class TargetNotFound(Exception):
+    """
+    Exception raised when the search on a_list does not find a target value.
+    """
+
+def binary_search(a_list, target):
+  """
+  Searches a_list for an occurrence of target
+  If found, returns the index of its position in the list
+  If not found, returns -1, indicating the target value isn't in the list
+  """
+  first = 0
+  last = len(a_list) - 1
+  while first <= last:
+    middle = (first + last) // 2
+    if a_list[middle] == target:
+      return middle
+    if a_list[middle] > target:
+      last = middle - 1
+    else:
+      first = middle + 1
+  return TargetNotFound(f"Value '{target}' not found in the list.")
+
+# try:
+#     index = binary_search([1, 3, 5, 7, 9], 4)
+#     print(f"Target found at index: {index}")
+# except TargetNotFound as e:
+#     print(e)
